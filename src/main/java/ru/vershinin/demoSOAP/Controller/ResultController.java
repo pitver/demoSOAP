7e9f8881c096f994c84bd3b01443dbe8a1c00ebd
pitver <pitver2014@yandex.ru>
2021-03-28 22:23:11 +0300

    24.03.2021 - add async.Message AMQ

--- src/main/java/ru/vershinin/demoSOAP/Controller/ResultController.java
+++ src/main/java/ru/vershinin/demoSOAP/Controller/ResultController.java
@@ -1,25 +1,49 @@
 package ru.vershinin.demoSOAP.Controller;
 
 import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.vershinin.demoSOAP.calculate.wsdl.AddResponse;
 import ru.vershinin.demoSOAP.calculate.wsdl.DivideResponse;
 import ru.vershinin.demoSOAP.calculate.wsdl.MultiplyResponse;
 import ru.vershinin.demoSOAP.calculate.wsdl.SubtractResponse;
+/*import ru.vershinin.demoSOAP.client.Receiver;*/
+import ru.vershinin.demoSOAP.client.Sender;
 import ru.vershinin.demoSOAP.model.Result;
 import ru.vershinin.demoSOAP.service.ResultService;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.util.UUID;
 
 
 @Api(description = "Калькулятор, поодерживаемые операции: сложение, вычитание, умножение, деление")
 @RestController
 public class ResultController {
+
+    private final Sender sender;
+
+/*    private final Receiver receiver;*/
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private Destination statusDestination;
+
+
     final
     ResultService resultService;
 
-    public ResultController(ResultService resultService) {
+    public ResultController(ResultService resultService, Sender sender) {
         this.resultService = resultService;
+        this.sender = sender;
+
     }
 
     public static final String paternFormat = "\\d+";
@@ -28,39 +52,56 @@ public class ResultController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "successful operation", response = Result.class),
             @ApiResponse(code = 400, message = "Invalid input"),})
+
     @GetMapping("/additions")
-    public Result add(
+    public String add(
             @ApiParam(value = "Cannot empty and use only numbers.")
 
             @RequestParam(value = "num1", required = false, defaultValue = "1") String num1,
+
             // @PathVariable @NotBlank @Pattern(regexp = "\\d") int num1,
             @ApiParam(value = "Cannot empty and use only numbers.")
-            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) {
+            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) throws JMSException {
         //  @PathVariable @NotBlank @Pattern(regexp = "\\d") int num2) {
 
         AddResponse response;
         if (num1.matches(paternFormat) && (num2.matches(paternFormat))) {
+
             response = resultService.add(Integer.parseInt(num1), Integer.parseInt(num2));
         } else {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Invalid input");
         }
         int res = response.getAddResult();
-        return new Result(res);
+
+        return getCorrelationId(res);
     }
+
+    @GetMapping("/getres")
+    public String subtraction(
+
+            @RequestParam(value = "correlationId", required = false, defaultValue = "1") String correlationId) {
+
+
+        String status = sender.receiveOrderStatus(correlationId);
+        System.out.println(status);
+
+        return status;
+    }
+
 
     @ApiOperation(value = "Вычитание")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "successful operation", response = Result.class),
             @ApiResponse(code = 400, message = "Invalid input"),})
     @GetMapping("/subtraction")
-    public Result subtraction(
+    public String subtraction(
             @ApiParam(value = "Cannot empty and use only numbers.")
 
             @RequestParam(value = "num1", required = false, defaultValue = "1") String num1,
             // @PathVariable @NotBlank @Pattern(regexp = "\\d") int num1,
             @ApiParam(value = "Cannot empty and use only numbers.")
-            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) {
+            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) throws JMSException {
         //  @PathVariable @NotBlank @Pattern(regexp = "\\d") int num2) {
         SubtractResponse response;
         if (num1.matches(paternFormat) && (num2.matches(paternFormat))) {
@@ -70,7 +111,8 @@ public class ResultController {
                     HttpStatus.BAD_REQUEST, "Invalid input");
         }
         int res = response.getSubtractResult();
-        return new Result(res);
+        return getCorrelationId(res);
+
     }
 
     @ApiOperation(value = "Умножение")
@@ -78,13 +120,13 @@ public class ResultController {
             @ApiResponse(code = 200, message = "successful operation", response = Result.class),
             @ApiResponse(code = 400, message = "Invalid input"),})
     @GetMapping("/multiply")
-    public Result multiply(
+    public String multiply(
             @ApiParam(value = "Cannot empty and use only numbers.")
 
             @RequestParam(value = "num1", required = false, defaultValue = "1") String num1,
             // @PathVariable @NotBlank @Pattern(regexp = "\\d") int num1,
             @ApiParam(value = "Cannot empty and use only numbers.")
-            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) {
+            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) throws JMSException {
         //  @PathVariable @NotBlank @Pattern(regexp = "\\d") int num2) {
         MultiplyResponse response;
         if (num1.matches(paternFormat) && (num2.matches(paternFormat))) {
@@ -94,7 +136,7 @@ public class ResultController {
                     HttpStatus.BAD_REQUEST, "Invalid input");
         }
         int res = response.getMultiplyResult();
-        return new Result(res);
+        return getCorrelationId(res);
     }
 
     @ApiOperation(value = "Деление")
@@ -102,13 +144,13 @@ public class ResultController {
             @ApiResponse(code = 200, message = "successful operation", response = Result.class),
             @ApiResponse(code = 400, message = "Invalid input"),})
     @GetMapping("/division")
-    public Result division(
+    public String division(
             @ApiParam(value = "Cannot empty and use only numbers.")
 
             @RequestParam(value = "num1", required = false, defaultValue = "1") String num1,
             // @PathVariable @NotBlank @Pattern(regexp = "\\d") int num1,
             @ApiParam(value = "Cannot empty and use only numbers.")
-            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) {
+            @RequestParam(value = "num2", required = false, defaultValue = "1") String num2) throws JMSException {
         //  @PathVariable @NotBlank @Pattern(regexp = "\\d") int num2) {
         DivideResponse response;
         if (num1.matches(paternFormat) && (num2.matches(paternFormat)) && Integer.parseInt(num2) != 0) {
@@ -118,6 +160,21 @@ public class ResultController {
                     HttpStatus.BAD_REQUEST, "Invalid input");
         }
         int res = response.getDivideResult();
-        return new Result(res);
+        return getCorrelationId(res);
+    }
+    @JmsListener(destination = "${destination.order}")
+    private String getCorrelationId(int res) throws JMSException {
+        String correlationId = sender.sendOrder(UUID.randomUUID().toString());
+
+
+        System.out.println(correlationId);
+
+        jmsTemplate.send(statusDestination, (Session messageCreator) -> {
+            TextMessage message =
+                    messageCreator.createTextMessage(String.valueOf(res));
+            message.setJMSCorrelationID(correlationId);
+            return message;
+        });
+        return correlationId;
     }
 }
---
+++ src/main/java/ru/vershinin/demoSOAP/config/SenderConfig.java
@@ -0,0 +1,62 @@
+package ru.vershinin.demoSOAP.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.Destination;
+
+@Configuration
+public class SenderConfig {
+
+    @Value("${activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${destination.order}")
+    private String orderDestination;
+
+    @Value("${destination.status}")
+    private String statusDestination;
+
+    @Bean
+    public ActiveMQConnectionFactory senderConnectionFactory() {
+        ActiveMQConnectionFactory activeMQConnectionFactory =
+                new ActiveMQConnectionFactory();
+        activeMQConnectionFactory.setBrokerURL(brokerUrl);
+
+        return activeMQConnectionFactory;
+    }
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory =
+                new CachingConnectionFactory(senderConnectionFactory());
+        cachingConnectionFactory.setSessionCacheSize(10);
+
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public Destination orderDestination() {
+        return new ActiveMQQueue(orderDestination);
+    }
+
+    @Bean
+    public Destination statusDestination() {
+        return new ActiveMQQueue(statusDestination);
+    }
+
+    @Bean
+    public JmsTemplate orderJmsTemplate() {
+        JmsTemplate jmsTemplate =
+                new JmsTemplate(cachingConnectionFactory());
+        jmsTemplate.setDefaultDestination(orderDestination());
+        jmsTemplate.setReceiveTimeout(2000);
+
+        return jmsTemplate;
+    }
+}
